f98e9122f46534d555b39b81073eaa1ed86b79cc
vishalsvaghasiya <vishal.genextwebs@gmail.com>
2021-08-24 10:34:37 +0530

    Nesting Componets in props

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ class App extends Component {
                 <h1>My First React Class base code</h1>
                 <h1>Welcome :)</h1>
                 <Ninjas/>
+                <Ninjas name="visha" age="26" belt="white"/>
+                <Ninjas name="isha" age="36" belt="pink"/>
             </div>
         )
     }
--- src/Ninjas.js
+++ src/Ninjas.js
@@ -2,12 +2,30 @@ import React, {Component} from "react";
 
 class Ninjas extends Component {
     render() {
+        // todo: 1. get props
+        console.log(this.props)
+        const {name, age, belt} = this.props;
         return (
             <div className="ninja">
-                <h1>Info</h1>
+                {/*Todo: Static Use Props 0*/}
+                <h1>Static Use Info</h1>
                 <h4>Name: Vishal</h4>
                 <h4>Age: 24</h4>
                 <h4>Belt: Black</h4>
+
+                {/*Todo: Use Props 2*/}
+                <h1>Use Props 2 Info</h1>
+                <h4>Name: {this.props.name}</h4>
+                <h4>Age: {this.props.age}</h4>
+                <h4>Belt: {this.props.belt}</h4>
+
+                {/*Todo: Use Props 3 */}
+                <h1>Use Props 3 Info</h1>
+                <h4>Name: {name}</h4>
+                <h4>Age: {age}</h4>
+                <h4>Belt: {belt}</h4>
+                <hr/>
+                
             </div>
         );
     }
